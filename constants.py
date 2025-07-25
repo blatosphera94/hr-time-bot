880d6295daadac83f921bf97bf381b845e44b331
blatosphera94 <youremail@example.com>
2025-07-25 12:21:12 +0700

    NEW

---
+++ constants.py
@@ -0,0 +1,5 @@
+# Файл: constants.py
+# Этот файл хранит общие константы, чтобы избежать циклических импортов.
+
+# Состояния для диалогов ConversationHandler
+GET_DATES_TEXT, GET_REPORT_DATES, GET_LOCATION, GET_USERS_FILE = range(4)
